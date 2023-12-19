2fa4b5186c0f41075ab0ec716364caee2349a3f2
Konstantin Teplykh <K.Teplykh@student.tudelft.nl>
2023-12-19 14:35:47 +0100

    Started creation of the courier endpoints, but got issues with authorization

--- delivery-microservice/src/main/java/nl/tudelft/sem/template/example/Application.java
+++ delivery-microservice/src/main/java/nl/tudelft/sem/template/example/Application.java
@@ -2,13 +2,18 @@ package nl.tudelft.sem.template.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * Example microservice application.
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class, UserDetailsServiceAutoConfiguration.class})
+@ComponentScan(basePackages = "nl.tudelft.sem.template.example")
 public class Application {
     public static void main(String[] args) {
+        System.out.println("Starting...");
         SpringApplication.run(Application.class, args);
     }
 }
---
+++ delivery-microservice/src/main/java/nl/tudelft/sem/template/example/config/SecurityConfig.java
@@ -0,0 +1,36 @@
+package nl.tudelft.sem.template.example.config;
+
+import nl.tudelft.sem.template.example.authentication.AuthenticationFilter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        System.out.println("!!!!I am here!!!!");
+        http.csrf()
+                .disable()
+                .authorizeRequests()
+                .antMatchers("**/**")
+                .authenticated()
+                .and()
+                .httpBasic()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .addFilterBefore(new AuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+    }
+
+}
