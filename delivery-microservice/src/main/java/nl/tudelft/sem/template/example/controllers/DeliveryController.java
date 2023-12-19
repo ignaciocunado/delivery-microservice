687e296e5d707c48bd6a8ed6386fa04c84078392
Stanislaw <s.a.ostyk-narbutt@student.tudelft.nl>
2023-12-19 14:01:36 +0100

    import interface from generated code

--- delivery-microservice/src/main/java/nl/tudelft/sem/template/example/controllers/DeliveryController.java
+++ delivery-microservice/src/main/java/nl/tudelft/sem/template/example/controllers/DeliveryController.java
@@ -1,2 +1,23 @@
-package nl.tudelft.sem.template.example.controllers;public class DeliveryController {
+package nl.tudelft.sem.template.example.controllers;
+
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+
+// delivery api from generated yaml
+import nl.tudelft.sem.api.DeliveryApi;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.UUID;
+
+public class DeliveryController implements DeliveryApi {
+    // implement acceptDelivery
+    @Override
+    public ResponseEntity<Void> acceptDelivery(@Parameter(name = "deliveryID", description = "ID of the delivery to mark as accepted.", required = true, in = ParameterIn.PATH) @PathVariable("deliveryID") UUID deliveryID) {
+        // get delivrey by deliveryID
+
+        return new ResponseEntity(HttpStatus.NOT_IMPLEMENTED);
+    }
 }
