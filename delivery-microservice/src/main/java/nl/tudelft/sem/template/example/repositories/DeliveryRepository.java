b9d2c792be00ccc2f2df9adb4ceffe722a0fc8d4
lsionescu <ionescu-8@student.tudelft.nl>
2023-12-20 01:02:26 +0200

    repositories and incipient version of the method for issue #28

---
+++ delivery-microservice/src/main/java/nl/tudelft/sem/template/example/repositories/DeliveryRepository.java
@@ -0,0 +1,9 @@
+package nl.tudelft.sem.template.example.repositories;
+
+import nl.tudelft.sem.model.Delivery;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface DeliveryRepository extends JpaRepository<Delivery, UUID> {
+}
