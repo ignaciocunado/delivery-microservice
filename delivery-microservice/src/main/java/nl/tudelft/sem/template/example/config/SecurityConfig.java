b64cf4788baa19d5308904d76512e1f5e405f7b8
Konstantin Teplykh <K.Teplykh@student.tudelft.nl>
2023-12-21 19:20:07 +0100

    Added the authorization part

--- delivery-microservice/src/main/java/nl/tudelft/sem/template/example/config/SecurityConfig.java
+++ delivery-microservice/src/main/java/nl/tudelft/sem/template/example/config/SecurityConfig.java
@@ -1,24 +1,19 @@
 package nl.tudelft.sem.template.example.config;
 
-import nl.tudelft.sem.template.example.authentication.AuthenticationFilter;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import nl.tudelft.sem.template.example.authorization.AuthorizationFilter;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.DefaultSecurityFilterChain;
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
-        System.out.println("!!!!I am here!!!!");
         http.csrf()
                 .disable()
                 .authorizeRequests()
@@ -30,7 +25,7 @@ public class SecurityConfig extends SecurityConfigurerAdapter<DefaultSecurityFil
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
-                .addFilterBefore(new AuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+                .addFilterBefore(new AuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 
 }
