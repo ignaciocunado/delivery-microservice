dc4583609552c851a804bb12d4faab75dbc1c9fa
Maxime <mxcaux@protonmail.com>
2023-12-22 14:25:41 +0100

    OpenAPI annotations, H2 not accessible yet

--- delivery-microservice/src/main/java/nl/tudelft/sem/template/example/database/DeliveryRepository.java
+++ delivery-microservice/src/main/java/nl/tudelft/sem/template/example/database/DeliveryRepository.java
@@ -1,6 +1,6 @@
 package nl.tudelft.sem.template.example.database;
 
-import nl.tudelft.sem.template.example.entities.Delivery;
+import nl.tudelft.sem.model.Delivery;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DeliveryRepository extends JpaRepository<Delivery, String> {}
