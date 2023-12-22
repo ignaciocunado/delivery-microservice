9828a9d05f96e1dec654776b327e6149eae31e57
Maxime <mxcaux@protonmail.com>
2023-12-22 14:44:16 +0100

    Update entity annotations

--- delivery-microservice/src/main/java/nl/tudelft/sem/template/example/Application.java
+++ delivery-microservice/src/main/java/nl/tudelft/sem/template/example/Application.java
@@ -2,6 +2,7 @@ package nl.tudelft.sem.template.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServic
  * Example microservice application.
  */
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class, UserDetailsServiceAutoConfiguration.class})
+@EntityScan("nl.tudelft.sem.model")
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
