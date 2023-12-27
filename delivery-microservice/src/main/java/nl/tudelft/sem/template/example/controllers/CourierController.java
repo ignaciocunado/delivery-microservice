b63e1340b4d560da5b14afe250667a4669ab16f8
Stanislaw <s.a.ostyk-narbutt@student.tudelft.nl>
2023-12-27 10:32:05 +0100

    Basic structure of controllers and subcontrollers.

--- delivery-microservice/src/main/java/nl/tudelft/sem/template/example/controllers/CourierController.java
+++ delivery-microservice/src/main/java/nl/tudelft/sem/template/example/controllers/CourierController.java
@@ -1,19 +1,19 @@
 package nl.tudelft.sem.template.example.controllers;
 
 import java.util.UUID;
-import nl.tudelft.sem.api.DeliveryApi;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class CourierController implements DeliveryApi {
+/**
+ * Sub controller of DeliveryController. Handles requests from couriers.
+ * Note: Remember to define methods here and add them in DeliveryController.
+ */
+public class CourierController  {
 
     private boolean checkCourier(String role) {
         return role.equals("courier");
     }
 
-    @Override
     public ResponseEntity<String> getPickUpLocation(UUID deliveryId, String role) {
         if (checkCourier(role)) {
             return new ResponseEntity<>("PickUp location is 123.321.666", HttpStatus.OK);
--- delivery-microservice/src/main/java/nl/tudelft/sem/template/example/controllers/DeliveryController.java
+++ delivery-microservice/src/main/java/nl/tudelft/sem/template/example/controllers/DeliveryController.java
@@ -9,15 +9,20 @@ import nl.tudelft.sem.api.DeliveryApi;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
 
+/**
+ * Main Delivery Controller. Calls on other controllers to handle requests.
+ * Note: Add methods here to integrate them into the API from other subcontrollers.
+ */
+@RestController
 public class DeliveryController implements DeliveryApi {
-    // implement acceptDelivery
-    @Override
-    public ResponseEntity<Void> acceptDelivery(@Parameter(name = "deliveryID", description = "ID of the delivery to mark as accepted.", required = true, in = ParameterIn.PATH) @PathVariable("deliveryID") UUID deliveryID) {
-        // get delivrey by deliveryID
+    private final transient CourierController courierController = new CourierController();
 
-        return new ResponseEntity(HttpStatus.NOT_IMPLEMENTED);
+    @Override
+    public ResponseEntity<String> getPickUpLocation(UUID deliveryId, String role) {
+        return courierController.getPickUpLocation(deliveryId, role);
     }
 }
