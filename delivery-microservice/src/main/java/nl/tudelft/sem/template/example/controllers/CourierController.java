f5111420343d9051d06d015f0f53695c123ecbc5
Stanislaw <s.a.ostyk-narbutt@student.tudelft.nl>
2023-12-27 16:18:35 +0100

    Add dependency injection for easy testing with mockito

--- delivery-microservice/src/main/java/nl/tudelft/sem/template/example/controllers/CourierController.java
+++ delivery-microservice/src/main/java/nl/tudelft/sem/template/example/controllers/CourierController.java
@@ -3,11 +3,13 @@ package nl.tudelft.sem.template.example.controllers;
 import java.util.UUID;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 
 /**
  * Sub controller of DeliveryController. Handles requests from couriers.
  * Note: Remember to define methods here and add them in DeliveryController.
  */
+@Component
 public class CourierController  {
 
     private boolean checkCourier(String role) {
--- delivery-microservice/src/main/java/nl/tudelft/sem/template/example/controllers/DeliveryController.java
+++ delivery-microservice/src/main/java/nl/tudelft/sem/template/example/controllers/DeliveryController.java
@@ -6,6 +6,8 @@ import io.swagger.v3.oas.annotations.enums.ParameterIn;
 // delivery api from generated yaml
 import nl.tudelft.sem.api.DeliveryApi;
 
+import org.hibernate.service.spi.InjectService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,8 +21,17 @@ import java.util.UUID;
  */
 @RestController
 public class DeliveryController implements DeliveryApi {
-    private final transient CourierController courierController = new CourierController();
+    private final transient CourierController courierController;
 
+    /**
+     * Constructor for DeliveryController.
+     * @param courierController CourierController to handle courier requests
+     */
+    // tag for dependency injection of courier controller
+    @Autowired
+    public DeliveryController(CourierController courierController) {
+        this.courierController = courierController;
+    }
     @Override
     public ResponseEntity<String> getPickUpLocation(UUID deliveryId, String role) {
         return courierController.getPickUpLocation(deliveryId, role);
