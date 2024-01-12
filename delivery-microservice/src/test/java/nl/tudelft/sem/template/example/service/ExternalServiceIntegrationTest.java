90d14679052cd9f155db1215c5073897447a98b0
Stanislaw <s.a.ostyk-narbutt@student.tudelft.nl>
2024-01-12 20:05:26 +0100

    full testing for authorization external service

---
+++ delivery-microservice/src/test/java/nl/tudelft/sem/template/example/service/ExternalServiceIntegrationTest.java
@@ -0,0 +1,86 @@
+package nl.tudelft.sem.template.example.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.web.client.RestTemplate;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.UUID;
+
+@SpringBootTest
+@ActiveProfiles("integration")
+public class ExternalServiceIntegrationTest {
+
+    private transient ExternalService externalService;
+    private transient RestTemplate restTemplate;
+
+
+    @BeforeEach
+    void setUp() {
+        restTemplate = Mockito.mock(RestTemplate.class);
+        externalService = new ExternalServiceActual(restTemplate, "a", "b");
+    }
+
+    @Test
+    void getRestaurantLocation() {
+        Mockito.when(restTemplate.getForObject(Mockito.anyString(), Mockito.any())).thenReturn("PickUp in format xxx.xxx");
+
+        assert(externalService.getRestaurantLocation(UUID.randomUUID()).equals("PickUp in format xxx.xxx"));
+    }
+
+    @Test
+    void getOrderDestination() {
+        Mockito.when(restTemplate.getForObject(Mockito.anyString(), Mockito.any())).thenReturn("Destination in format xxx.xxx");
+
+        assert(externalService.getOrderDestination(UUID.randomUUID(), UUID.randomUUID()).equals("Destination in format xxx.xxx"));
+    }
+
+    @Test
+    void verifyNoRole() {
+        boolean result = externalService.verify("123", "nothing");
+        assertFalse(result);
+    }
+
+    @Test
+    void verifyVendorRole() {
+        boolean result = externalService.verify("123", "vendor");
+
+        Mockito.when(restTemplate.getForEntity(Mockito.anyString(), Mockito.any())).thenReturn(new ResponseEntity<>(null, null, HttpStatus.OK));
+
+        assertTrue(result);
+    }
+
+    @Test
+    void verifyCourierRole() {
+        boolean result = externalService.verify("123", "courier");
+
+        Mockito.when(restTemplate.getForEntity(Mockito.anyString(), Mockito.any())).thenReturn(new ResponseEntity<>(null, null, HttpStatus.OK));
+
+        assertTrue(result);
+    }
+
+    @Test
+    void verifyAdminRole() {
+        boolean result = externalService.verify("123", "admin");
+
+        Mockito.when(restTemplate.getForEntity(Mockito.anyString(), Mockito.any())).thenReturn(new ResponseEntity<>(null, null, HttpStatus.OK));
+
+        assertTrue(result);
+    }
+
+    @Test
+    void verifyValidRoleNon200ErrorCode() {
+        boolean result = externalService.verify("123", "admin");
+
+        Mockito.when(restTemplate.getForEntity(Mockito.anyString(), Mockito.any())).thenReturn(new ResponseEntity<>(null, null, HttpStatus.UNAUTHORIZED));
+
+        assertFalse(result);
+    }
+
+}
--- delivery-microservice/src/test/java/nl/tudelft/sem/template/example/service/ExternalServiceTest.java
+++ delivery-microservice/src/test/java/nl/tudelft/sem/template/example/service/ExternalServiceTest.java
@@ -3,12 +3,17 @@ package nl.tudelft.sem.template.example.service;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@SpringBootTest
+@ActiveProfiles("test")
 public class ExternalServiceTest {
+
 
     @Autowired
     private ExternalService externalService;
