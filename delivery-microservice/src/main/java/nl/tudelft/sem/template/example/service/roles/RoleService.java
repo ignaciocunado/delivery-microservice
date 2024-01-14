9787f6d52572cc741a9b1168ccd64c78e09c914d
Maxime Caux <j.m.caux@student.tudelft.nl>
2024-01-14 14:28:10 +0100

    Clarify interface comment

--- delivery-microservice/src/main/java/nl/tudelft/sem/template/example/service/roles/RoleService.java
+++ delivery-microservice/src/main/java/nl/tudelft/sem/template/example/service/roles/RoleService.java
@@ -6,7 +6,7 @@ import java.util.function.Supplier;
 
 public interface RoleService {
     /**
-     * Checks if the user is authorized to access the controller.
+     * Checks if the user is authorized to access actions of a given role.
      * @param role the role of the user
      */
     <T> ResponseEntity<T> checkAndHandle(String role, Supplier<ResponseEntity<T>> operation);
