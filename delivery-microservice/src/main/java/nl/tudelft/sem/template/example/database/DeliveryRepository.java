80e0fe1e459da8505ff719b94477259ff90bff10
Konstantin Teplykh <K.Teplykh@student.tudelft.nl>
2024-01-15 20:27:16 +0100

    Added @Repository annotation to databeses

--- delivery-microservice/src/main/java/nl/tudelft/sem/template/example/database/DeliveryRepository.java
+++ delivery-microservice/src/main/java/nl/tudelft/sem/template/example/database/DeliveryRepository.java
@@ -3,6 +3,7 @@ package nl.tudelft.sem.template.example.database;
 import java.util.UUID;
 import nl.tudelft.sem.model.Delivery;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-
+@Repository
 public interface DeliveryRepository extends JpaRepository<Delivery, UUID> {}
